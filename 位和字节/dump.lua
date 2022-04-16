f25659316e742aa6279edb7f39f2670b9e5f8ba3
jiangpengyong <jiangpengyong@joyy.com>
2022-04-16 14:08:55 +0800

    feat: 位和字节

--- 位和字节/dump.lua
+++ 位和字节/dump.lua
@@ -1,11 +1,15 @@
+-- 江澎涌
 local f = assert(io.open(arg[1], "rb"))
+-- 16 字节读取文件
 local blockSize = 16
+-- 循环读取文件，每次读取 16 个字符
 for bytes in f:lines(blockSize) do
+    -- 循环每个字节
     for i = 1, #bytes do
         local b = string.unpack("B", bytes, i)
         io.write(string.format("%02X ", b))
     end
-    io.write(string.rep("    ", blockSize - #bytes))
+    io.write(string.rep("   ", blockSize - #bytes))
     bytes = string.gsub(bytes, "%c", ".")
     io.write(" ", bytes, "\n")
 end
