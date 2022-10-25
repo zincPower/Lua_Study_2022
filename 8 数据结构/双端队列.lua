815d7abef9e0a9e4134f8cfc41af807178a2bee1
jiangpengyong <jiangpengyong@joyy.com>
2022-10-25 09:03:54 +0800

    feat: 双端队列

--- 8 数据结构/双端队列.lua
+++ 8 数据结构/双端队列.lua
@@ -13,3 +13,31 @@ function pushFirst(list, value)
     list.first = first
     list[first] = value
 end
+
+function pushLast(list, value)
+    local last = list.last + 1
+    list.last = last
+    list[last] = value
+end
+
+function popFirst(list)
+    local first = list.first
+    if first > list.last then
+        error("list is empty")
+    end
+    local value = list[first]
+    list[first] = nil
+    list.first = first + 1
+    return value
+end
+
+function popLast(list)
+    local last = list.last
+    if list.first > last then
+        error("list is empty")
+    end
+    local value = list[last]
+    list[last] = nil
+    list.last = last - 1
+    return value
+end
